dfcbe7ea9fa013b40196017f2415cfc2326ccbc6
Nicolas Urdiales <nico.urdiales15@gmail.com>
2022-12-20 14:46:02 -0300

    Calculo 50% de descuento

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-let monto = Number(prompt("Ingrese un monto"))
-let opcion = 0
-do {
-    opcion = Number(prompt("Ingrese un valor para calcular sus cuotas \n Ingrese 1 para calcular 3 cuotas \n Ingrese 2 para calcular 6 cuotas \n Ingrese 3 para calcular 9 cuotas \n Ingrese 0 para salir"))
-    if (opcion == 1){
-        alert("3 cuotas de : " + monto / 3)
-    } else if (opcion == 2) {
-        alert("6 cuotas de : " + monto/6)
-    } else if (opcion == 3) {
-        alert("9 cuotas de : " + monto / 9)
-    } else if (opcion != 0) {
-        alert("El numero ingresado es incorrecto")
-    }
-} while (opcion != 0)
+let productos = [
+    {id:1 , nombre: "pelota" , precio: Number(prompt("Ingrese un precio:"))},
+    {id:2 , nombre: "botines" , precio: Number(prompt("Ingrese un precio:"))},
+    {id:3 , nombre: "medias" , precio: Number(prompt("Ingrese un precio:"))},
+    {id:4 , nombre: "short" , precio: Number(prompt("Ingrese un precio: ")) },
+]
+let porcentaje = productos.map((producto) => {
+    producto.precio = (producto.precio * 0.50).toFixed(2)
+    return producto.precio
+})
+alert("los precios con 50% de descuento: " + porcentaje)
